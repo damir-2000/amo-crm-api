8c5a3918a7e0636c7a8210af51f0b6910ecb2aa9
Damir <damir.gilemhonov.00@mail.ru>
2024-02-19 20:37:33 +0500

    0.1.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as readme_file:
 requirements = ["pydantic>==2.6.0", "PyJWT>==2.8.0", "requests>==2.31.0"]
 
 setup(
-    name="amo-crm-api",
+    name="amo_crm_api",
     version="0.1.1",
     author="damir-2000",
     author_email="gilemhonov.damir@gmail.com",
@@ -14,7 +14,7 @@ setup(
     long_description=readme,
     long_description_content_type="text/markdown",
     url="https://github.com/damir-2000/amo_crm_api",
-    packages=find_packages("./amo-crm-api"),
+    packages=["amo_crm_api"],
     install_requires=requirements,
     license="MIT",
     python_requires=">=3.11",
