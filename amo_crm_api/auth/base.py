841eaa68656709164a4993fbe45b69247f209c69
Damir <damir.gilemhonov.00@mail.ru>
2024-10-14 20:06:18 +0500

    fix readTimeout

--- amo_crm_api/auth/base.py
+++ amo_crm_api/auth/base.py
@@ -2,7 +2,7 @@ from abc import ABC, abstractmethod
 from time import sleep
 from typing import Literal, Optional, Union
 
-from requests import Response, models, request
+from requests import Response, models, request, exceptions
 
 from ..exceptions import (
     DoesNotExist,
@@ -27,32 +27,48 @@ class BaseAuth(ABC):
         params: Optional[dict] = None,
         data: Optional[dict] = None,
         json: Optional[Union[dict, list]] = None,
+        tried: int = 0,
     ) -> Response:
-        url = url + path if url else self._url + self._api_v + path
-        sleep(0.2)
-        response = request(
-            method=method,
-            url=url,
-            params=params,
-            data=data,
-            json=json,
-            auth=self._auth,
-            timeout=5,
-        )
+        request_url = url + path if url else self._url + self._api_v + path
+        sleep(0.1)
+        try:
+            response = request(
+                method=method,
+                url=request_url,
+                params=params,
+                data=data,
+                json=json,
+                auth=self._auth,
+                timeout=5,
+            )
+        except exceptions.ReadTimeout:
+            if tried <= 5:
+                self.request(
+                    method=method,
+                    path=path,
+                    url=url,
+                    params=params,
+                    data=data,
+                    json=json,
+                    tried=tried + 1,
+                )
+            else:
+                raise exceptions.ReadTimeout()
+
         if response.status_code == 404:
-            raise DoesNotExist
+            raise DoesNotExist()
 
         elif response.status_code == 401:
-            raise AuthenticationError
+            raise AuthenticationError()
 
         elif response.status_code == 400:
             raise ValidationError(response.json())
 
         elif response.status_code == 403:
-            raise AccountBlockedError
+            raise AccountBlockedError()
 
         elif response.status_code == 429:
-            raise LimitExceededError
+            raise LimitExceededError()
 
         return response
 
